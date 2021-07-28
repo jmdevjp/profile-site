dd1c42a7481f821fcffc92d2d897c10ca248e958
jmdevjp <88482250+jmdevjp@users.noreply.github.com>
2021-07-28 18:16:13 +0900

    1つ目の記事を作成

--- article/post.php
+++ article/post.php
@@ -7,38 +7,49 @@ $body = htmlspecialchars($_POST['body'], ENT_QUOTES, 'UTF-8');
 $date = date("Y年m月d日 H:i:s");
 $fname = date("Ymd") . '.html';
 
+$page = '';
+$page .= '<!DOCTYPE html><html lang="ja">';
+
+// 共通の <head> を変数に追加する。
+ob_start();
+include '../head-common.php';
+$page .= ob_get_clean();
+
+$page .= '<body>';
+$page .= '<header class="site-header">';
+$page .= '<h1><a href="../index.html" class="site-header-top-link">jmdevjp\'s profile site</a></h1>';
+$page .= '<nav class="site-header-nav">';
+$page .= '<li>ブログ</li>';
+$page .= '<li><a href="#">その他</a></li>';
+$page .= '</nav>';
+$page .= '</header>';
+
+$page .= '<main>';
+$page .= '<header class="article-header">';
+$page .= '<h2>' . $title .'</h2>';
+$page .= '<p>' . $date . '</p>';
+$page .= '</header>';
+$page .= '<p class="article-summary">';
+$page .= $summary;
+$page .= '</p>';
+$page .= '<div class="article-body">';
+
+
+$page .= htmlspecialchars(nl2br($_POST['body']), ENT_QUOTES, 'UTF-8');
+$page .= '</div>';
+$page .= '</main>';
+
+$page .= '<footer class="site-footer">';
+$page .= '<p>&copy; 2021- <a href="https://twitter.com/jmdevjp" target="_blank" rel="noopener noreferrer">@jmdevjp</a></p>';
+$page .= '</footer>';
+$page .= '</body>';
+$page .= '</html>';
+
+$fh = fopen('./' . $fname, 'w');
+fwrite($fh, $page);
+fclose($fh);
+
+// 記事一覧に遷移する。
+header('Location: ./' . $fname);
+
 ?>
-
-<!DOCTYPE html>
-<html lang="ja">
-
-<!-- 共通の <head> 取り込み -->
-<?php include '../head-common.php'; ?>
-
-<!-- 共通のページヘッダはひとまずベタ書き -->
-<header class="site-header">
-    <h1><a href="../index.html" class="site-header-top-link">jmdevjp's profile site</a></h1>
-    <nav class="site-header-nav">
-        <li>ブログ</li>
-        <li><a href="#">その他</a></li>
-    </nav>
-</header>
-
-<main>
-    <header class="article-header">
-        <h2><?php echo $title; ?></h2>
-        <p><?php echo $date; ?></p>
-    </header>
-    <p class="article-summary">
-        <?php echo $summary; ?>
-    </p>
-    <p class="article-body">
-        <?php echo $body; ?>
-    </p>
-</main>
-
-<footer class="site-footer">
-    <p>&copy; 2021- <a href="https://twitter.com/jmdevjp" target="_blank" rel="noopener noreferrer">@jmdevjp</a></p>
-</footer>
-</body>
-</html>
