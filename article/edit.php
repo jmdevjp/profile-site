e53aba4c09318e6288799903cd8502fde5446e06
jmdevjp <88482250+jmdevjp@users.noreply.github.com>
2021-07-31 23:45:22 +0900

    記事編集画面を作成

---
+++ article/edit.php
@@ -0,0 +1,60 @@
+<?php
+
+$db_file = '../database.csv';
+$db_fh = new SplFileObject($db_file, "r");
+$db_fh->setFlags(SplFileObject::READ_CSV);
+
+$id = $_POST['id'];
+
+for (; !$db_fh->eof(); $db_fh->next())
+{
+    $edit_line = $db_fh->current();
+    if ($edit_line[0] === $id)
+    {
+        $title = $edit_line[1];
+        $summary = $edit_line[3];
+        $body = $edit_line[4];
+        break;
+    }
+}
+
+$file = null;
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <title>jmdevjp's profile site</title>
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+
+        <!-- CSS -->
+        <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
+        <link href="../css/style.css" rel="stylesheet" >
+    </head>
+
+    <body>
+        <header class="site-header">
+            <h1><a href="../index.html" class="site-header-top-link">jmdevjp's profile site</a></h1>
+            <nav class="site-header-nav">
+                <li><a href="./top.php">ブログ</a></li>
+                <li><a href="#">その他</a></li>
+            </nav>
+        </header>
+
+        <form method="POST" action="edit_done.php">
+            <input type="hidden" name="id" value="<?php echo $id; ?>">
+            <label class="post-label" for="title">タイトル</label><br>
+            <input class="post-input" type="text" name="title" value="<?php echo htmlspecialchars($title, ENT_QUOTES, 'UTF-8'); ?>"><br>
+            <label class="post-label" for="summary">概要</label><br>
+            <textarea class="post-input post-summary" type="text" name="summary"><?php echo htmlspecialchars($summary, ENT_QUOTES, 'UTF-8'); ?></textarea><br>
+            <label class="post-label" for="body">本文</label><br>
+            <textarea class="post-input post-body" type="text" name="body"><?php echo htmlspecialchars($body, ENT_QUOTES, 'UTF-8'); ?></textarea><br>
+            <label class="post-label" for="send"></label><br>
+            <button class="post-button" type="submit" name="send">送信</button>
+        </form>
+    </body>
+</html>
--- article/top.php
+++ article/top.php
@@ -7,7 +7,7 @@ use League\CommonMark\CommonMarkConverter;
 $filepath = '../database.csv';
 $file = new SplFileObject($filepath, "r");
 $file->setFlags(SplFileObject::READ_CSV);
-$COLUMN_SIZE  = 4;
+$COLUMN_SIZE  = 5;
 
 $converter = new CommonMarkConverter([
     'html_input' => 'strip',
@@ -38,14 +38,19 @@ $converter = new CommonMarkConverter([
                     <?php if (count($line) == $COLUMN_SIZE) { ?>
                     <article class="article">
                         <header class="article-header">
-                            <h2><?php echo $converter->convertToHtml($line[0]); ?></h2>
-                            <p><?php echo $line[1]; ?></p>
+                            <h2>#<?php echo $line[0]; ?> <?php echo $converter->convertToHtml($line[1]); ?></h2>
+                            <p><?php echo $line[2]; ?></p>
+                            <form method="POST" action="edit.php">
+                                <input type="hidden" name="id" value="<?php echo $line[0]; ?>">
+                                <label class="post-label" for="edit"></label><br>
+                                <button class="post-button" type="submit" name="edit">編集</button>
+                            </form>
                         </header>
                         <div class="article-summary">
-                            <?php echo $converter->convertToHtml($line[2]); ?>
+                            <?php echo $converter->convertToHtml($line[3]); ?>
                         </div>
                         <div class="article-body">
-                            <?php echo $converter->convertToHtml($line[3]); ?>
+                            <?php echo $converter->convertToHtml($line[4]); ?>
                         </div>
                     </article>
                     <?php } ?>
