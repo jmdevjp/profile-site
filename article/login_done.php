b5126e38faf9042ad65975405882b85b0f84d8db
jmdevjp <88482250+jmdevjp@users.noreply.github.com>
2021-08-04 09:59:18 +0900

    ハッシュ化したパスワードを使って認証する処理を追加

--- article/login_done.php
+++ article/login_done.php
@@ -2,8 +2,19 @@
 require_once './common.php';
 
 $user_id = $_POST[USER_ID];
+$password = $_POST['password'];
 
-if ($user_id === 'user')
+$file_path = '../data/user';
+$lines = file($file_path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+if (count($lines) !== 2)
+{
+    SetError('不明なエラー。管理者に連絡してください。');
+    header('Location: ./login.php');
+    exit;
+}
+
+if (password_verify($user_id, $lines[0]) && password_verify($password, $lines[1]))
 {
     session_regenerate_id(true);
     SetUserId($user_id);
