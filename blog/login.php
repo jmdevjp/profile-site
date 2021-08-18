c5fa5b082b2684466a375320efec75009005c765
jmdevjp <88482250+jmdevjp@users.noreply.github.com>
2021-08-18 16:58:12 +0900

    ログイン画面を装飾

--- blog/login.php
+++ blog/login.php
@@ -6,20 +6,22 @@ require_once './common.php';
     <?php include('./head-common.php'); ?>
     <body>
         <?php include('./pageheader-common.php'); ?>
-        <?php if (IsLogin()) { ?>
-            <p>ログイン済みです。</P>
-            <a href="./top.php">ブログトップ</a>
-        <?php } else { ?>
-            <?php if (AnyErrors()) { ?>
-                <p><strong><?php echo GetErrorMessage(); ?></strong></p>
+        <div class="wrapper">
+            <?php if (IsLogin()) { ?>
+                <p>ログイン済みです。</P>
+                <a href="./top.php">ブログトップ</a>
+            <?php } else { ?>
+                <?php if (AnyErrors()) { ?>
+                    <p><strong><?php echo GetErrorMessage(); ?></strong></p>
+                <?php } ?>
+                <form method="POST" action="login_done.php" class="login-form">
+                    <label for="<?php echo USER_ID; ?>">ユーザーID</label>
+                    <input type="text" name="<?php echo USER_ID; ?>" class="input">
+                    <label for="password">パスワード</label>
+                    <input type="password" name="password" class="input">
+                    <p><button type="submit" name="login" class="button">ログイン</button></p>
+                </form>
             <?php } ?>
-            <form method="POST" action="login_done.php">
-                <p><label for="<?php echo USER_ID; ?>">ユーザーID</label>
-                <input type="text" name="<?php echo USER_ID; ?>"></p>
-                <p><label for="password">パスワード</label>
-                <input type="password" name="password"></p>
-                <p><button type="submit" name="login">ログイン</button></p>
-            </form>
-        <?php } ?>
+        </div>
     </body>
 <html>
