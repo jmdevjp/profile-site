9b5082b0b3488bb60c29b5d8e5fac0ad47c310a8
jmdevjp <88482250+jmdevjp@users.noreply.github.com>
2021-08-20 16:44:19 +0900

    OGPを設定

--- blog/head-common.php
+++ blog/head-common.php
@@ -8,4 +8,13 @@
     <!-- CSS -->
     <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
     <link href="./css/style.css" rel="stylesheet" >
+
+    <!-- OGP -->
+    <meta property="og:url" content="http://pika.chu.jp/blog.top">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="jmdevjp's blog">
+    <meta property="og:image" content="http://pika.chu.jp/profile.png">
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="@jmdevjp" />
+    <meta name="twitter:player" content="@jmdevjp" />
 </head>
