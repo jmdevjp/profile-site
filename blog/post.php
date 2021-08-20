f7c562ca7f807f98434b075050f5ffb756725466
jmdevjp <88482250+jmdevjp@users.noreply.github.com>
2021-08-20 14:51:57 +0900

    投稿ページの装飾を設定

--- blog/post.php
+++ blog/post.php
@@ -17,15 +17,15 @@ if (!IsLogin())
     <body>
         <?php include('./pageheader-common.php'); ?>
 
-        <form method="POST" action="post_done.php">
-            <label class="post-label" for="title">タイトル</label><br>
-            <input class="post-input" type="text" name="title"><br>
-            <label class="post-label" for="summary">概要</label><br>
-            <textarea class="post-input post-summary" type="text" name="summary"></textarea><br>
-            <label class="post-label" for="body">本文</label><br>
-            <textarea class="post-input post-body" type="text" name="body"></textarea><br>
-            <label class="post-label" for="send"></label><br>
-            <button class="post-button" type="submit" name="send">投稿</button>
+        <form method="POST" action="post_done.php" class="post-form wrapper">
+            <label for="title">タイトル</label>
+            <input type="text" name="title">
+            <label for="summary">概要</label>
+            <textarea class="post-summary" type="text" name="summary"></textarea>
+            <label for="body">本文</label>
+            <textarea class="post-body" type="text" name="body"></textarea>
+            <label for="send"></label>
+            <button class="button" type="submit" name="send">投稿</button>
         </form>
     </body>
 </html>
